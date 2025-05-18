dfa09a3ad0ac584eede145dd38f16966840dc1cc
CodeScanf <100558509+CodeScanf@users.noreply.github.com>
2025-05-18 23:37:14 +0800

    插件Api的定义 --xiaoli

---
+++ my_plugin_api/src/main/java/tech/insight/plugin/MyPlugin.java
@@ -0,0 +1,12 @@
+package tech.insight.plugin;
+
+/**
+ * @author lijiaobin
+ * @version 1.0
+ * @project java_basic
+ * @description
+ * @date 2025/5/17 21:36:03
+ */
+public interface MyPlugin {
+    void beforeGetTime();
+}
