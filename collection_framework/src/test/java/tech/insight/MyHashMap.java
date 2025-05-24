b53a19ccca750714d06254e95e9dd9c3f4bda822
CodeScanf <100558509+CodeScanf@users.noreply.github.com>
2025-05-24 14:48:17 +0800

    HashMap的实现 --xiaoli

---
+++ collection_framework/src/test/java/tech/insight/MyHashMap.java
@@ -0,0 +1,38 @@
+package tech.insight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * @author lijiaobin
+ * @version 1.0
+ * @project java_basic
+ * @description
+ * @date 2025/5/24 14:10:13
+ */
+public class MyHashMap {
+
+    @Test
+//    void testApi(){
+//        MyHashMap<String, String> myHashMap = new MyHashMap<>();
+//        int count = 100000;
+//        for (int i = 0; i < count; i++) {
+//            myHashMap.put(String.valueOf(i), String.valueOf(i));
+//        }
+//
+//        assertEquals(count, myHashMap.size());
+//
+//        for (int i = 0; i < count; i++) {
+//            assertEquals(String.valueOf(i), myHashMap.get(String.valueOf(i)));
+//        }
+//
+//        myHashMap.remove("8");
+//        assertNull(myHashMap.get("8"));
+//
+//        assertEquals(count - 1, myHashMap.size());
+//    }
+
+
+}
